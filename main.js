e2d677acb26547e7d13106f9f17f580f5ad03086
rasma123 <82595793+rasma123@users.noreply.github.com>
2021-10-21 17:39:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="dexi ";
+var last_name="debella"
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+var a="20";
+var b="17";
+var add=a+b;
+console.log(add);
+var c="34";
+var e="12";
+console.log(c);
+console.log(e);
